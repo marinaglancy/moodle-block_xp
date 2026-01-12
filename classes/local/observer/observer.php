dd83e52e9c31a573523b490b1a502649c36d6e42
Marina Glancy <marina.glancy@gmail.com>
2026-01-12 20:45:20 +0000

    Add full stop in the end of the URL comment.
    Claude fixed it too but I wanted to commit it separately.

--- classes/local/observer/observer.php
+++ classes/local/observer/observer.php
@@ -14,7 +14,7 @@
 // You should have received a copy of the GNU General Public License
 // along with Level Up XP.  If not, see <https://www.gnu.org/licenses/>.
 //
-// https://levelup.plus
+// https://levelup.plus.
 
 /**
  * Block XP observer.
